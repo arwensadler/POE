4142753dea42427ede919ab37bb64b551af9680e
Arwen Sadler <asadler@olin.edu>
2019-10-11 10:48:47 -0400

    Create POE_lab_3.ino

---
+++ POE_lab_3/POE_lab_3.ino
@@ -0,0 +1,16 @@
+// Code for POE Lab 3: a line-following robot.
+// By @arwensadler
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
