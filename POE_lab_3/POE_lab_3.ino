44ea20e70243865d34374cedd1256ef1fdd9ce4d
Arwen Sadler <asadler@olin.edu>
2019-10-13 19:02:31 -0400

    Update POE_lab_3.ino
    Added set-up code and comments

--- POE_lab_3/POE_lab_3.ino
+++ POE_lab_3/POE_lab_3.ino
@@ -1,16 +1,34 @@
 // Code for POE Lab 3: a line-following robot.
 // By @arwensadler
 
+
+// Setting up the motors:
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_DCMotor *myMotorleft = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotorright = AFMS.getMotor(4);
+
+// Setting up the sensors:
+int leftSensor = A0;
+int rightSensor = A1;
+
 void setup() {
-  // put your setup code here, to run once:
+  AFMS.begin();
+
+  Serial.begin(9600);
+  Serial.print("a: left speed adjustment");
+  Serial.println("b: right speed adjustment");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  myMotorleft->setSpeed(50);
+  myMotorright->setSpeed(50);
+  myMotorleft->run(FORWARD);
+  myMotorright->run(FORWARD);
 
 }
