9d82498a9db015f1462af519e11cf91b35db315d
Arwen Sadler <asadler@olin.edu>
2019-10-14 11:50:21 -0400

    Update POE_lab_3.ino
    Scoped down to only have one input variable of "speed"

--- POE_lab_3/POE_lab_3.ino
+++ POE_lab_3/POE_lab_3.ino
@@ -43,27 +43,12 @@ int b = 20;
 int c = 20;
 int d = 1;
 int e = 1;
+int speed = 10;
 
 
 void loop() {
   recInput();
-  parseInput();
-  updateInput();
-/*
-  if (Serial.available() > 0) {
-    a = Serial.read();
-    Serial.println(a);
-    if(Serial.read() == 'a'){
-      a = Serial.parseInt();
-      Serial.println(a);
-    }
-    // TO DEBUG: The second if statement isn't running
-    else if(Serial.read() == 'b'){
-      b = Serial.parseInt();
-      Serial.println(b);
-    }
-  }
-  */
+
   if (leftSensor && rightSensor >= a) {
     b = b+d;
     c = c+e;
@@ -90,45 +75,8 @@ void loop() {
 }
 
 void recInput() {
-  static byte ndx = 0;
-  char endMarker = '\n';
-  char rc;
-  while (Serial.available() > 0 && newData == false) {
-    rc = Serial.read();
-
-    if (rc != endMarker) {
-      input[ndx] = rc;
-      ndx++;
-      if (ndx >= numChars) {
-        ndx = numChars - 1;
-      }
-    } else {
-      input[ndx] = '\0'; // end the string
-      ndx = 0;
-      newData = true;
+   if (Serial.available() > 0) {    // is a character available?
+      speed = Serial.readString().toInt(); // read the number and set it to speed
+      Serial.println(speed);
     }
-    
-  }
-}
-void parseInput(){
-  if (newData == true) {
-    char * strtokIndx;
-  
-    strtokIndx = strtok(input,"="); // Get the first part (the string)
-    strcpy(charInput, strtokIndx); //Copy it to charInput
-  
-    strtokIndx = strtok(NULL, "="); // Continue where the last left off
-    intInput = atoi(strtokIndx); // Convert to int
-  }
-  
-}
-
-void updateInput(){
-  if (newData == true) {
-    Serial.println("Received variable:");
-    Serial.println(charInput);
-    Serial.println("Received int:");
-    Serial.println(intInput);
-    newData = false;
-  }
 }
